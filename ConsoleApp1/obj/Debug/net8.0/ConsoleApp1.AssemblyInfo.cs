fac502aa3d90f2b3692b57e3d0d630d9822d7ce0
Trond Håvard Thune <trond.havard@excite.no>
2024-07-30 14:01:18 +0200

    Delete TestClass.cs

--- ConsoleApp1/obj/Debug/net8.0/ConsoleApp1.AssemblyInfo.cs
+++ ConsoleApp1/obj/Debug/net8.0/ConsoleApp1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dc7f17d94dbc63b9a0fd3a7732cf0e4c6d64a07d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9b6fcf12d0630059fa536ba50bd1971632ad2c2d")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
