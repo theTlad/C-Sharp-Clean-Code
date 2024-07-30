d3344b4ed253963c346f6323ba62c7ab267dbc9e
Trond Håvard Thune <trond.havard@excite.no>
2024-07-30 09:38:57 +0200

    Naming conventions

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -4,6 +4,20 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+    }
+
+    class CustomerService //PascalCase
+    {
+        public const int MAX_CUSTOMERS = 100; // ALL_CAPS for const
+        
+        public int CustomerCount { get; set; } // PascalCase on public property
+
+        private string lastCustomerName = "John"; // camelCase for private property
+
+        public string GetCustomerName(int customerId) // PascalCase for method, camelCase for parameter
+        {
+            string customerName = "John Doe"; // camelCase for internal variable
+            return customerName;
+        }
     }
 }
