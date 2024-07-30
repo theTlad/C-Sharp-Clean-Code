b19423b410296d3daec02a03213e0aafe625c781
Trond Håvard Thune <trond.havard@excite.no>
2024-07-30 10:00:29 +0200

    Meaningful namings: Is, Get, Set, Has, Can

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -29,6 +29,21 @@ class Program
         {
             return _customerName;
         }
+
+        // Meaningful namings: Is, Get, Set, Has, Can
+        public void SetCustomerName()
+        {
+        }
+
+        public bool HasErrors()
+        {
+            return false;
+        }
+
+        public bool CanReceiveEmails()
+        {
+            return false;
+        }
     }
 
     class OrderProcessor // nouns for class names
@@ -36,13 +51,13 @@ class Program
         // Use "has" or "is" for boolean values
         private bool hasErrors = false;
         private bool isValid = true;
-        
+
         // Use PascalCase for methods
         public bool IsValid()
         {
             return true;
         }
-        
+
         // Use PascalCase for properties
         public bool HasErrors { get; set; }
 
